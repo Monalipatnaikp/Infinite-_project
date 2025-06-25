f121b768e0e78c92c9114aa10d0071633e53972d
Monali Patnaik <monalip@infinite.com>
2025-06-25 18:11:04 +0530

    coding

--- csharp/code challenge/code challenge1/code challenge1/Program.cs
+++ csharp/code challenge/code challenge1/code challenge1/Program.cs
@@ -1,78 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace code_challenge1
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main()
+        Console.WriteLine("Remove character at given position");
+        Console.Write("Enter a word: ");
+        string word = Console.ReadLine();
+
+        Console.Write("Enter the position : ");
+        int index = Convert.ToInt32(Console.ReadLine());
+
+        if (index >= 0 && index < word.Length)
         {
-
-            Console.WriteLine("string");
-
-            string word = "Python";
-            string result1 = word.Remove(1, 1);
-            Console.WriteLine("After removing 1 " + result1);
-            string result2 = word.Remove(0, 1);
-            Console.WriteLine("After removing  2: " + result2);
-            string result3 = word.Remove(4, 1);
-            Console.WriteLine("After removing  3: " + result3);
-
-           Console.WriteLine("\n Swap  ");
-           string input1 = "abcd";
-            string input2 = "a";
-            string input3 = "xy";
-            Console.WriteLine("original: " + input1 + "  Swap: " + SwapFirstLast(input1));
-            Console.WriteLine("Original: " + input2 + "  Swap: " + SwapFirstLast(input2));
-            Console.WriteLine("Original: " + input3 + "  Swap: " + SwapFirstLast(input3));
-
-             Console.WriteLine("\n large number ");
-            FindLargest(1, 2, 3);
-            FindLargest(1, 3, 2);
-            FindLargest(1, 1, 1);
-            FindLargest(1, 2, 2);
+            string newWord = word.Remove(index, 1);
+            Console.WriteLine("Result: " + newWord);
         }
-          static string SwapFirstLast(string str)
+        else
         {
-            if (str.Length <= 1)
-            {
-                return str; 
-            }
-
-            char first = str[0];
-            char last = str[str.Length - 1];
-            string middle = str.Substring(1, str.Length - 2);
-            string newWord = last + middle + first;
-            return newWord;
+            Console.WriteLine("Invalid index.");
         }
-      static void FindLargest(int num1, int num2, int num3)
-        {
-            int biggest = num1;
 
-            if (num2 > biggest)
-            {
-                biggest = num2;
-            }
-            if (num3 > biggest)
-            {
-                biggest = num3;
-            }
-            Console.WriteLine("Numbers: " + num1 + ", " + num2 + ", " + num3 + " largest is: " + biggest);
-            Console.ReadLine();
-            {
+     
+        Console.WriteLine("\n  Swap first and last character");
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
 
-            }
-        }
+        string swapped = SwapFirstLast(input);
+        Console.WriteLine("Result: " + swapped);
+
+      
+        Console.WriteLine("\n  Find largest among three numbers ");
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        int num3 = Convert.ToInt32(Console.ReadLine());
+
+        int largest = FindLargest(num1, num2, num3);
+        Console.WriteLine("The largest number is: " + largest);
+    }
+
+    static string SwapFirstLast(string str)
+    {
+        if (str.Length <= 1)
+            return str;
+
+        char first = str[0];
+        char last = str[str.Length - 1];
+        string middle = str.Substring(1, str.Length - 2);
+
+        return last + middle + first;
+    }
+
+    static int FindLargest(int a, int b, int c)
+    {
+        int max = a;
+
+        if (b > max)
+            max = b;
+
+        if (c > max)
+            max = c;
+
+        return max;
+        Console.ReadLine();
     }
 }
-        
-    
-
-
-
-
-
 
