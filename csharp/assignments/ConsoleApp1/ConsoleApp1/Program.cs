fefbef66f0bd3d3444b32026e619091753a136ab
Monali Patnaik <monalip@infinite.com>
2025-06-30 16:57:55 +0530

    assignment

---
+++ csharp/assignments/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace MobilePhoneNotification
+{
+    
+    class MobilePhone
+    {
+        
+        public delegate void RingEventHandler();
+
+        public event RingEventHandler OnRing;
+
+        
+        public void ReceiveCall()
+        {
+            Console.WriteLine("\n Incoming Call");
+            OnRing?.Invoke(); 
+        }
+    }
+
+    
+    class RingtonePlayer
+    {
+        public void PlayRingtone()
+        {
+            Console.WriteLine("Playing ringtone");
+        }
+    }
+
+    
+    class ScreenDisplay
+    {
+        public void ShowCallerInfo()
+        {
+            Console.WriteLine(" Displaying caller information");
+        }
+    }
+
+    
+    class VibrationMotor
+    {
+        public void Vibrate()
+        {
+            Console.WriteLine("Phone is vibrating");
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            MobilePhone phone = new MobilePhone();
+            RingtonePlayer ringtone = new RingtonePlayer();
+            ScreenDisplay display = new ScreenDisplay();
+            VibrationMotor vibration = new VibrationMotor();
+            phone.OnRing += ringtone.PlayRingtone;
+            phone.OnRing += display.ShowCallerInfo;
+            phone.OnRing += vibration.Vibrate;
+
+            while (true)
+            {
+                
+                string input = Console.ReadLine()?.Trim().ToLower();
+
+                if (input == "call")
+                {
+                    phone.ReceiveCall();
+                }
+                else if (input == "exit")
+                {
+                    Console.WriteLine(" Exit");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(" Invalid input");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
